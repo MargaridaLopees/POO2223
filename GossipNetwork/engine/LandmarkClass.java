f64e11f80e56bd807eea3876fce9b924023671ae
Margarida Lopes <maargaridalopes@gmail.com>
2025-10-06 16:53:26 +0100

    Update LandmarkClass.java

--- GossipNetwork/engine/LandmarkClass.java
+++ GossipNetwork/engine/LandmarkClass.java
@@ -1,7 +1,7 @@
 /**
  * LandmarkClass class - implements the Interface Landmark.
  *
- * @authors Margrida Lopes n64557 and Diogo Castelos n66045
+ * @authors Margrida Lopes n64557
  */
 
 package engine;
